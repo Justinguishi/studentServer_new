c8f6e4538eda999f452774b265965ec7121e0bac
Claire Ellul <ucfscde@ucl.ac.uk>
2019-07-21 09:52:12 -0400

    server all function check

--- studentServer.js
+++ studentServer.js
@@ -3,68 +3,12 @@ var express = require('express');
 var path = require("path"); 
 var app = express(); 
 
-
-var bodyParser = require('body-parser'); 
-app.use(bodyParser.urlencoded({ 
-  extended: true 
-})); 
-app.use(bodyParser.json()); 
-
-var fs = require('fs'); 
-var pg = require('pg'); 
- 
-var configtext = 
-""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js"); 
- 
-// now convert the configruation file into the correct format -i.e. a name/value pair array 
-var configarray = configtext.split(","); 
-var config = {}; 
-for (var i = 0; i < configarray.length; i++) { 
-    var split = configarray[i].split(':'); 
-    config[split[0].trim()] = split[1].trim(); 
-} 
- 
-var pool = new pg.Pool(config); 
-
-
-app.get('/postgistest',function(req,res){
-
-	pool.connect(function(err,client,done){
-
-		if (err){
-
-			console.log('not able to get connection'+err);
-
-			res.status(400).send(err);
-
-		}
-
-		client.query('SELECT name FROM london_poi',function(err,result){
-
-			done();
-
-			if (err){
-
-				console.log(err);
-
-				res.status(400).send(err);
-
-			}
-
-			res.status(200).send(result.rows);
-
-		});
-
-	});
-
-});
-
 // add an http server to serve files to the Edge browser  
  // due to certificate issues it rejects the https files if they are not 
  // directly called in a typed URL 
-var http = require('http'); 
-var httpServer = http.createServer(app);  
-httpServer.listen(4480); 
+ var http = require('http'); 
+ var httpServer = http.createServer(app);  
+ httpServer.listen(4480); 
  
 app.get('/:fileName', function (req, res) { 
   // run some server-side code 
@@ -74,7 +18,10 @@ app.get('/:fileName', function (req, res) {
  res.sendFile(__dirname + '/'+ fileName); 
 }); 
 
-
+ 
+ 
+ 
+ 
  // adding functionality to log the requests 
  app.use(function (req, res, next) { 
   var filename = path.basename(req.url); 
@@ -84,16 +31,6 @@ app.get('/:fileName', function (req, res) {
  }); 
 
  
- 
- app.use(function(req, res, next) { 
-   res.header("Access-Control-Allow-Origin", "*"); 
-   res.header("Access-Control-Allow-Headers", "X-Requested-With"); 
-   next(); 
- }); 
-
- 
- 
- 
-// serve static files - e.g. html, css 
+ // serve static files - e.g. html, css 
 // this should always be the last line in the server file 
 app.use(express.static(__dirname)); 
