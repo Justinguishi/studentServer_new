43ad18d7d5fb3000436ec968027e639f1b336110
Claire Ellul <ucfscde@ucl.ac.uk>
2019-07-21 00:22:31 -0400

    add variables

--- studentServer.js
+++ studentServer.js
@@ -9,12 +9,12 @@ var app = express();
  var httpServer = http.createServer(app);  
  httpServer.listen(4480); 
  
-
-  app.get('/test.html', function (req, res) { 
+app.get('/:fileName', function (req, res) { 
   // run some server-side code 
- console.log('test.html requested');  
- // note that __dirname  gives the path to the studentServer.js file 
- res.sendFile(__dirname + '/test.html'); 
+   var fileName = req.params.fileName; 
+ console.log(fileName + ' requested');  
+  // note that __dirname  gives the path to the studentServer.js file 
+ res.sendFile(__dirname + '/'+ fileName); 
 }); 
 
 
